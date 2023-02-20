d872531b9e164639c3ea9d3adefeaec4cc7ee7b4
Dev1321 <125664041+Dev1321@users.noreply.github.com>
2023-02-20 19:59:22 +0500

    Ad mob project added

---
+++ Assets/Ads.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+public class Ads : MonoBehaviour
+{
+    BannerView BannerView;
+    private  InterstitialAd interstitial;
+    private string _adUnitId = "ca-app-pub-3940256099942544/1033173712";
+
+
+
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+
+      
+       RequestInterstitial();
+    }
+
+ public void RunInterstitialAd()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+            RequestInterstitial();
+        }
+        else
+        {
+            RequestInterstitial();
+        }
+    }
+
+  
+    
+  public void RequestBanner()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#endif
+        this.BannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        var adRequest = new AdRequest.Builder()
+            .AddKeyword("unity-admob-sample")
+            .Build();
+
+        Debug.Log("Loading banner ad.");
+        BannerView.LoadAd(adRequest);
+    }
+    private void RequestInterstitial()
+    {
+        this.interstitial = new InterstitialAd(_adUnitId);
+        AdRequest request = new AdRequest.Builder().Build();
+        this.interstitial.LoadAd(request);
+    }
+}
+
+
+
